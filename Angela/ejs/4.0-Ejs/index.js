fa5e8ec8838dc2da08acda651c0b12e0ff487aa0
nitheeshmk5 <22107041@srcac.ac.in>
2024-06-19 08:59:07 +0530

    add ejs files

---
+++ Angela/ejs/4.0-Ejs/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+
+const app = express();
+
+const date = new Date();
+const day = date.getDay();
+
+let days = " ";
+let advise = "";
+if (day < 5) {
+  days = "Weekday";
+  advise = "Work hard";
+} else {
+  days = "Weekend";
+  advise = "Lets Enjoy !";
+}
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { today: days, advise: advise });
+});
+
+app.listen(8000, () => {
+  console.log("ON");
+});
